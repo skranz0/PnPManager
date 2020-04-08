cb107d58f0dfc5dddcf2f2af982fd8ff8247e446
Stefan Kranz <kranz.stefan99@tutanota.com>
2020-04-08 16:46:48 +0200

    optimize imports

--- app/src/main/java/skranz/pnpmanager/MainActivity.kt
+++ app/src/main/java/skranz/pnpmanager/MainActivity.kt
@@ -1,13 +1,10 @@
 package skranz.pnpmanager
 
 import android.content.Intent
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
