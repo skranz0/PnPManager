8566caa40d21ce6323e9a70fc7cfe117f8a9a2ce
Stefan Kranz <kranz.stefan99@tutanota.com>
2020-04-08 17:46:07 +0200

    values are transferred, shared prefs dunked

--- app/src/main/java/skranz/pnpmanager/MainActivity.kt
+++ app/src/main/java/skranz/pnpmanager/MainActivity.kt
@@ -2,30 +2,25 @@ package skranz.pnpmanager
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    var mob: Mob? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun addMob(view: View) {
+    fun addMob() {
         val name = findViewById<EditText>(R.id.mobName).text.toString()
         val health = findViewById<EditText>(R.id.mobHealth).text.toString()
         val armor = findViewById<EditText>(R.id.mobArmor).text.toString()
 
-        mob = Mob(this)
-        mob!!.NAME = name
-        mob!!.HEALTH = health
-        mob!!.ARMOR = armor
-
         val myMonster = Intent(this, MeinMonster::class.java)
+        myMonster.putExtra("Name", name)
+        myMonster.putExtra("Health", health.toInt())
+        myMonster.putExtra("Armor", armor.toInt())
         startActivity(myMonster)
     }
 }
--- app/src/main/java/skranz/pnpmanager/MeinMonster.kt
+++ app/src/main/java/skranz/pnpmanager/MeinMonster.kt
@@ -10,17 +10,14 @@ class MeinMonster : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mein_monster)
 
-        val mobvals = getSharedPreferences("skranz.pnpmanager.mobvals.prefs", 0)
+        //val mobvals = getSharedPreferences("skranz.pnpmanager.mobvals.prefs", 0)
 
-        val name = if (mobvals.contains("name"))
-            mobvals.getString("name", null) else "Monster"
-        val health = if (mobvals.contains("health"))
-            mobvals.getInt("name", 20) else 20
-        val armor = if (mobvals.contains("armor"))
-            mobvals.getInt("armor", 0) else 0
+        val name = intent.getStringExtra("Name")
+        val health = intent.getIntExtra("Health", -2)
+        val armor = intent.getIntExtra("Armor", -2)
 
-        tvName.text = name
-        tvHealth.text = health.toString()
-        tvArmor.text = armor.toString()
+        tvName.text = "Name: $name"
+        tvHealth.text = "LeP: $health"
+        tvArmor.text = "RS: $armor"
     }
 }
