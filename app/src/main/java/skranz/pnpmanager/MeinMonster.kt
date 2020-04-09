ea2a8978da5343bda9311b05457127b6d05d8cb1
Stefan Kranz <kranz.stefan99@tutanota.com>
2020-04-09 19:26:38 +0200

    WIP variable damage

--- app/src/main/java/skranz/pnpmanager/MeinMonster.kt
+++ app/src/main/java/skranz/pnpmanager/MeinMonster.kt
@@ -3,6 +3,7 @@ package skranz.pnpmanager
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_mein_monster.*
@@ -12,6 +13,8 @@ class MeinMonster : AppCompatActivity() {
     private var name: String = ""
     private var health: Int = 0
     private var armor: Int = 0
+
+    private var damageTaken = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +34,15 @@ class MeinMonster : AppCompatActivity() {
 
     private fun addDamage() {
         val attackDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val dialogLayout = inflater.inflate(R.layout.alert_dialog_with_edit_text, null)
+        val inputDamage = dialogLayout.findViewById<EditText>(R.id.editText).text.toString()
 
         with(attackDialog) {
             setTitle("$name angreifen")
-            setMessage("5 Schaden verursachen")
+            setView(dialogLayout)
             setNegativeButton("Abbruch") { _: DialogInterface, _: Int -> }
-            setPositiveButton("Okay") { _: DialogInterface, _: Int -> hit(5) }
+            setPositiveButton("Okay") { _: DialogInterface, _: Int -> damageTaken = inputDamage.toInt() }
             show()
         }
     }
@@ -53,8 +59,8 @@ class MeinMonster : AppCompatActivity() {
     }
 
     private fun hit(damage: Int) {
-        if( health + armor >= damage) {
-            health -= damage - armor
+        if( health + armor >= damageTaken) {
+            health -= damageTaken - armor
         } else {
             health = 0
         }
