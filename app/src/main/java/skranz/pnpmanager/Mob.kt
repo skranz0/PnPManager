2b4e99dadf522a1afbe755cf08a85abe21833351
Stefan Kranz <kranz.stefan99@tutanota.com>
2020-04-11 21:06:21 +0200

    remove redundant getter/setter

--- app/src/main/java/skranz/pnpmanager/Mob.kt
+++ app/src/main/java/skranz/pnpmanager/Mob.kt
@@ -2,14 +2,8 @@ package skranz.pnpmanager
 
 class Mob(name: String, health: Int, armor: Int) {
     val name: String
-        get() = field
     var health: Int = 0
-        get() = field
-        set(value) {
-            field = value
-        }
     val armor: Int
-        get() = field
 
     init { //make sure given values make sense
         this.name = if(name != "") name else "unknown"
